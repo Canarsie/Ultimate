5b90f01b1384fccedd65afc7e7c22bef4df8dc25
Robert <Brobertforrester@gmail.com>
2023-09-28 00:14:54 -0400

    explosion working

--- src/file.js
+++ src/file.js
@@ -1,18 +1,21 @@
-// // get all the elements on the page
-// const elements = document.querySelectorAll('*');
+// get all the elements on the page
+const elements = document.querySelectorAll('*');
 
-// // give every element an event listener that removes it on right click plus alt and shift key
-// for (let i = 0; i < elements.length; i++) {
-//   elements[i].addEventListener('contextmenu', (event) => {
-//     // console.log(event);
-//     if (event.altKey === true && event.shiftKey === true) event.target.remove();
-//   })
-  
-// }
-let contextMenuItem = {
-    "id": "removeElement",
-    "title": "Remove Element",
-    "contexts": ["selection"]
-    };
-  
-  chrome.contextMenus.create(contextMenuItem);
+//creating a function that will replace an element with a picture
+function replaceWithPicture(el){
+  const image = document.createElement (`img`);
+  image.src = 'https://patch.com/img/cdn20/shutterstock/24346758/20210210/015045/styles/patch_image/public/shutterstock-521147707___10134728239.jpg'
+  image.style.width = '50 px'
+  el.parentNode.replaceChild(image, el)
+}
+
+// give every element an event listener that removes it on right click plus alt and shift key
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener('contextmenu', (event) => {
+    // console.log(event);
+    if (event.altKey === true && event.shiftKey === true) {
+      event.preventDefault();
+      replaceWithPicture(event.target);
+    }
+  })
+}
